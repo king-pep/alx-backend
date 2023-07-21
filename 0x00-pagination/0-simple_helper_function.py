8d6494d53b16841be481e530a41def61ad5ffcc0
Abel Ben <abelben59@gmail.com>
2023-07-21 11:51:44 +0000

    0x00-pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
+""" Range simple helper fun """
+from typing import Tuple
 
-def index_range(page, page_size):
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
-    return start_index, end_index
 
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+        Range of the page
+
+        Args:
+            page: Current page
+            page_size: Total size of the page
+
+        Return:
+            tuple with the range start and end size page
+    """
+
+    final_size: int = page * page_size
+    start_size: int = final_size - page_size
+
+    return (start_size, final_size)
